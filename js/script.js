59f46c169a3d0cf2c2c5d5d01dd8608c7a3cc474
Tobi Williams <88983600+Tobi1019@users.noreply.github.com>
2023-04-30 22:02:06 +0100

    Update script.js

--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 var typed = new Typed(".typing",{
-    strings:["","Web Developer","Graphics Designer","Content Writer","Product Designer","Kdramalover :)"],
+    strings:["Graphics Designer","Content Writer","Product Designer","Kdramalover :)"],
     typespeed:90,
     BackSpeed:60,
     loop:true
